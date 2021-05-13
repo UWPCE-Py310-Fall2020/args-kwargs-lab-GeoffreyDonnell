649d4a8df0f21a754f1622d0085d584ac799af72
Geoffrey Donnell <geoffwd@uw.edu>
2021-05-13 10:04:46 -0700

    created test for function to develop

--- args_and_kwargs_lab.py
+++ args_and_kwargs_lab.py
@@ -1,1 +1,26 @@
+"""
+Write a function that has four optional parameters (with defaults):
+(1)fore_color
+(2)back_color
+(3)link_color
+(4)visited_color
+Have it return the colors (use strings for the colors, e.g. “blue”, “red”, etc.)
 
+Call it with a couple different parameters set. That is, write tests that verify that all of the following
+work as advertised:
+
+(1) Positional arguments:
+return_color('red', 'blue', 'yellow', 'chartreuse')
+
+(2) Using just keyword arguments:
+return_color(link_color='red', back_color='blue')
+
+(3) Using a combination of positional and keyword arguments:
+return_color('purple', link_color='red', back_color='blue')
+
+(4) Using *some_tuple and/or **some_dict
+    (4a) regular = ('red', 'blue')
+    (4b) links = {'link_color': 'chartreuse'}
+    (4c) return_color(*regular, **links)
+
+"""
---
+++ test_args_and_kwargs_lab.py
@@ -0,0 +1,43 @@
+"""
+
+Unit tests to test the return_color() function for the various type of arguments.
+
+(1) Positional arguments:
+return_color('red', 'blue', 'yellow', 'chartreuse')
+
+(2) Using just keyword arguments:
+return_color(link_color='red', back_color='blue')
+
+(3) Using a combination of positional and keyword arguments:
+return_color('purple', link_color='red', back_color='blue')
+
+(4) Using *some_tuple and/or **some_dict
+    (4a) regular = ('red', 'blue')
+    (4b) links = {'link_color': 'chartreuse'}
+    (4c) return_color(*regular, **links)
+
+"""
+
+from args_and_kwargs_lab import return_colors
+
+
+def test_positional_arguments():
+    expected = 'red', 'orange', 'yellow', 'green'
+    assert return_colors('red', 'orange', 'yellow', 'green') == expected
+
+
+def test_keyword_arguments():
+    expected = 'white', 'black', 'gray', 'silver'
+    kwargs = {'fore_color': 'white', 'back_color': 'black', 'link_color': 'gray', 'visited_color': 'silver'}
+    assert return_colors(**kwargs) == expected
+
+
+def test_combination_arguments():
+    expected = 'purple', 'blue', 'red', 'chartreuse'
+    assert return_colors('purple', link_color='red', back_color='blue') == expected
+
+def test_tuple_dict_arguments():
+    regular = ('red', 'blue')
+    links = {'link_color': 'chartreuse'}
+    expected = 'red', 'blue', 'chartreuse','chartreuse'
+    assert return_colors(*regular, **links) == expected
